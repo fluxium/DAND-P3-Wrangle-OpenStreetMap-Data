734bc8c357b45644bdc251d7a96bc49410e254f7
Mahlon Barrault <mahlonbarrault@gmail.com>
2016-03-12 19:46:56 -0700

    Adding additional code to clean postcodes in MongoDB. Both the audit and cleaning python scripts were altered to collect the malformed postcodes, produce a defaultdict(set), produce keys of bad postcodes with blank values. The dict was printed and then the correct postcodes were filled manually. There were only 33 so this took under 5 minutes.

--- Project/post_import_cleaning.py
+++ Project/post_import_cleaning.py
@@ -7,7 +7,7 @@ Created on Mon Aug 31 13:28:58 2015
 This file contains code to further clean data from the Zen Map Calgary Metro
 Extract OpenStreetMaps data.
 """
-
+import re
 
 # Instructor Code
 def get_db(db_name, server_name, username, password):
@@ -33,8 +33,39 @@ def aggregate(db, pipeline):
 def update_dirty_docs(db, changes):
     for c in changes.keys():
         db.DANDP3.update_one({'address.street' : c}, {'$set' : {'address.street' : changes[c]}})
+        
+def update_dirty_postcodes(db, changes):
+    for c in changes.keys():
+        db.DANDP3.update_many({'address.postcode' : c}, {'$set' : {'address.postcode' : changes[c]}})
 
-db = get_db('wrangling', '40.78.26.96:27017', 'docdbadmin', '')
+db = get_db('wrangling', '40.78.26.96:27017', 'docdbadmin', '***REMOVED***')
 
-changes = {'Rivercrest Drive South-east' : 'Rivercrest Drive SE'}
-update_dirty_docs(db, changes)
+street_changes = {'Rivercrest Drive South-east' : 'Rivercrest Drive SE'}
+update_dirty_docs(db, street_changes)
+
+postcode_changes = {u'T3a4b3': 'T3A 4B3', u'T1X1L8': 'T1X 1L8', \
+                    u'T3A0H7': 'T3A 0H7', u' T2J 0P8': 'T2J 0P8', \
+                    u'T3J4L8': 'T3J 4L8', u'T3N0E4': 'T3N 0E4', \
+                    u't3G 5T3': 'T3G 5T3', u'T3J0S3': 'T3J 0S3', \
+                    u'T2P3P8': 'T2P 3P8', u'T2E': '', u'T3N0A6': 'T3N 0A6', \
+                    u't3c2h6': 'T3C 2H6', u'T2G0H7': 'T2G 0H7', \
+                    u'AB T2S 2N1': 'T2S 2N1', u'T3A5R8': 'T3A 5R8', \
+                    u'AB T2G 2L2': 'T2G 2L2', u'1212': '', \
+                    u't2n 3P3': 'T2N 3P3', u'T3R0A1': 'T3R 0A1', \
+                    u'T3K-5P4': 'T3K 5P4', u'T2P0W3': 'T2P 0W3', \
+                    u'T2T0A7;T2T 0A7': 'T2T 0A7', u'T2R0E7': 'T2R 0E7', \
+                    u'T2J2T8': 'T2J 2T8', u'T2L1G1': 'T2L 1G1', \
+                    u'T3A6J1': 'T3A 6J1', u't2n 4l7': 'T2N 4L7', \
+                    u'T3G2V7': 'T3G 2V7', u'T3J0G7': 'T3J 0G7', \
+                    u'T2V2X3': 'T2V 2X3', u'T3B3X3': 'T3B 3X3', \
+                    u'T3R0H3': 'T3R 0H3', u'403-719-6250': ''}
+
+update_dirty_postcodes(db, postcode_changes)
+
+"""
+These are the problem postcodes from post_import_audit.py:
+
+{u'T3a4b3': 'T3A 4B3', u'T1X1L8': 'T1X 1L8', u'T3A0H7': 'T3A 0H7', u' T2J 0P8': 'T2J 0P8', u'T3J4L8': 'T3J 4L8', u'T3N0E4': 'T3N 0E4', u't3G 5T3': 'T3G 5T3', u'T3J0S3': 'T3J 0S3', u'T2P3P8': 'T2P 3P8', u'T2E': '', u'T3N0A6': 'T3N 0A6', u't3c2h6': 'T3C 2H6', u'T2G0H7': 'T2G 0H7', u'AB T2S 2N1': 'T2S 2N1', u'T3A5R8': 'T3A 5R8', u'AB T2G 2L2': 'T2G 2L2', u'1212': '', u't2n 3P3': 'T2N 3P3', u'T3R0A1': 'T3R 0A1', u'T3K-5P4': 'T3K 5P4', u'T2P0W3': 'T2P 0W3', u'T2T0A7;T2T 0A7': 'T2T 0A7', u'T2R0E7': 'T2R 0E7', u'T2J2T8': 'T2J 2T8', u'T2L1G1': 'T2L 1G1', u'T3A6J1': 'T3A 6J1', u't2n 4l7': 'T2N 4L7', u'T3G2V7': 'T3G 2V7', u'T3J0G7': 'T3J 0G7', u'T2V2X3': 'T2V 2X3', u'T3B3X3': 'T3B 3X3', u'T3R0H3': 'T3R 0H3', u'403-719-6250': ''}
+
+Fixes were imputted manually
+"""
